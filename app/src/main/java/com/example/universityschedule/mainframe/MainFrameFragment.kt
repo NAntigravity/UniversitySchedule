e8d057e123f6df9a7ff2321453598e4853c7461e
Nadezhda Zyatikova <amajski07@gmail.com>
2023-03-13 01:01:36 +0700

    Lesson item for lessons recycler added

---
+++ app/src/main/java/com/example/universityschedule/mainframe/MainFrameFragment.kt
@@ -0,0 +1,8 @@
+package com.example.universityschedule.mainframe
+
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+
+class MainFrameFragment: Fragment() {
+    private lateinit var recyclerView: RecyclerView
+}
